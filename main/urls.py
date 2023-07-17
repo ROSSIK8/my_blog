54dfd5521ca85aa3f2466a507b81f488d5de28b6
Sanzhay Ayurov <ayurov2004@gmail.com>
2023-07-17 18:01:44 +0900

    Change commit

--- main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import show_articles_categories, show_article, hello
-
-urlpatterns = [
-    path('hello', hello),
-    path('articles/', show_articles_categories),
-    path('article/<article>', show_article),
-]
